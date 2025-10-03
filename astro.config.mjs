40c606967932f1745c73ad737732a32825b1c92f
Eloy <22151220@aguascalientes.tecnm.mx>
2025-10-03 10:19:55 -0600

    Github Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: "https://github.com/Dalup-213.github.io",
+  base: "/Vacunacion",
 });
